a3b0cadff2d2d2ca0ce28d5e74bbdf3dae994f13
luisf <luisf@DESKTOP-P0D3UQL>
2023-04-10 20:02:36 -0700

    Luis HW

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -11,6 +11,17 @@ public class Day2Demo {
 	public void hubTest(){
 		System.out.println("test");
 	}
+	
+	public void LuisHW(){
+		System.out.println("first part");
+	}
+	
+	public void LuisHW2(){
+		System.out.println("first part2");
+	}
+	
+	
+	
 	/**
 	
 	This is coming from other team member...
